9adfe99432d255ec7d238559233e4c8f319f3184
Nishtha <24csu352@ncuindia.edu>
2025-11-13 09:47:58 +0530

    Add files via upload

---
+++ EXP7/TestImport.java
@@ -0,0 +1,8 @@
+import exercises.java.PackageDemo;
+
+public class TestImport {
+    public static void main(String[] args) {
+        PackageDemo obj = new PackageDemo();
+        obj.display();
+    }
+}
---
+++ EXP7/packagedemodriver.java
@@ -0,0 +1,8 @@
+package exercises.java;
+
+ class PackageDemoDriver {
+    public static void main(String[] args) {
+        PackageDemo obj = new PackageDemo();
+        obj.display();
+    }
+}
